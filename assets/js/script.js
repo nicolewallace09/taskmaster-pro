c3881be3fea9c259f8cb605663610178cc91011e
nicolewallace09 <nicole.elisaw@gmail.com>
2020-04-09 18:37:28 -0700

    drag/drop + delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,6 +142,71 @@ $(".list-group").on("blur", "input[type='text']", function (){
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerence: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+    
+     // add task data to the temp array as an object
+     tempArr.push({
+       text: text,
+       date: date
+     })
+});
+
+console.log(tempArr);
+
+var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+}
+
+});
+
+$('#trash').droppable({
+  accept: ".card .list-group-item",
+  tolerence: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -188,5 +253,4 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
 
